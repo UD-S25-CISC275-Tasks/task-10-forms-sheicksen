787289cce7a42be3bc2142e7da954600eacbc27e
Sophia Heicksen <heicksen@udel.edu>
2025-03-11 16:27:35 -0400

    Made CheckAnswer form with submit button.

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,32 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer,
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [answer, setAnswer] = useState("");
+    const [isCorrect, setCorrect] = useState("");
+    function updateAnswer(e: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(e.target.value);
+    }
+    function submit() {
+        if (answer === expectedAnswer) {
+            setCorrect("✔️");
+        } else {
+            setCorrect("❌");
+        }
+    }
     return (
-        <div>
+        <div style={{ textAlign: "center", alignItems: "center" }}>
             <h3>Check Answer</h3>
+            <Form.Group>
+                <Form.Label>Input Answer: </Form.Label>
+                <Form.Control onChange={updateAnswer}></Form.Control>
+                <Button onClick={submit}>Check Answer</Button>
+            </Form.Group>
+            {isCorrect}
         </div>
     );
 }
