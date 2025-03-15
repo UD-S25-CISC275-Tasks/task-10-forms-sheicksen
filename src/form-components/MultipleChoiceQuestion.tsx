dbe56d3fd787135036c35cc65ad6a07f1bf6f22a
Sophia Heicksen <heicksen@udel.edu>
2025-03-15 13:51:12 -0400

    Made MultipleChoiceQuestion Component.

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,29 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [selected, setSelected] = useState(options[0]);
+    console.log(options, expectedAnswer);
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group>
+                <Form.Label>
+                    Peasants dont get the question. Guess the answer:
+                </Form.Label>
+                <Form.Select
+                    value={selected}
+                    onChange={(e) => {
+                        setSelected(e.target.value);
+                    }}
+                >
+                    {options.map((option) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {selected === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
