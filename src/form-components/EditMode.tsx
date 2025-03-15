afe52523bfdc1de5b24b8b97072241bac88037af
Sophia Heicksen <heicksen@udel.edu>
2025-03-15 14:17:32 -0400

    Changed EditMode so that switch now hides the editing from (rather than just disabling it). Now passing tests.

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -4,7 +4,7 @@ import { Form } from "react-bootstrap";
 export function EditMode(): React.JSX.Element {
     const [isEditMode, setEditMode] = useState(false);
     const [name, setName] = useState("Your Name");
-    const [isStudent, setStudent] = useState(false);
+    const [isStudent, setStudent] = useState(true);
     function updateName(event: React.ChangeEvent<HTMLInputElement>) {
         setName(event.target.value);
     }
@@ -12,28 +12,34 @@ export function EditMode(): React.JSX.Element {
         <div>
             <h3>Edit Mode</h3>
             <Form.Check
-                id="edit-mode"
                 type="switch"
+                id="edit-mode"
                 label="Enable Edit Mode"
                 checked={isEditMode}
                 onChange={() => {
                     setEditMode(!isEditMode);
                 }}
-            ></Form.Check>
-            <Form.Group>
-                <Form.Control
-                    disabled={!isEditMode}
-                    onChange={updateName}
-                ></Form.Control>
-                <Form.Check
-                    disabled={!isEditMode}
-                    label="Student"
-                    checked={isStudent}
-                    onChange={() => {
-                        setStudent(!isStudent);
-                    }}
-                ></Form.Check>
-            </Form.Group>
+            />
+            {isEditMode && (
+                <Form.Group>
+                    <Form.Label>Name:</Form.Label>
+                    <Form.Control
+                        type="text"
+                        disabled={!isEditMode}
+                        onChange={updateName}
+                    ></Form.Control>
+                    <Form.Check
+                        disabled={!isEditMode}
+                        id="is-student"
+                        type="checkbox"
+                        label="Student"
+                        checked={isStudent}
+                        onChange={() => {
+                            setStudent(!isStudent);
+                        }}
+                    ></Form.Check>
+                </Form.Group>
+            )}
             {name} is {isStudent ? "" : "not"} a student.
         </div>
     );
