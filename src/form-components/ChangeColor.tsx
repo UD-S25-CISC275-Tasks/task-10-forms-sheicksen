9d09a16e0cb2c14b4201e78bbc86ceca5d1998c6
Sophia Heicksen <heicksen@udel.edu>
2025-03-14 23:08:41 -0400

    Fixed changed color to have data-testid. ChangeColor now passing all tests.

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,40 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
+const colors: string[] = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple",
+    "lightblue",
+    "coral",
+];
 export function ChangeColor(): React.JSX.Element {
+    const [color, setColor] = useState("red");
+    let colorButtons = colors.map((colour) => (
+        <Form.Check
+            inline
+            key={colour}
+            type="radio"
+            style={{ backgroundColor: colour }}
+            label={colour}
+            value={colour}
+            checked={color === colour}
+            onChange={(e) => {
+                setColor(e.target.value);
+            }}
+        ></Form.Check>
+    ));
     return (
         <div>
             <h3>Change Color</h3>
+            <Form.Group>{colorButtons}</Form.Group>
+            You have chosen{" "}
+            <span data-testid="colored-box" style={{ backgroundColor: color }}>
+                {color}
+            </span>
         </div>
     );
 }
